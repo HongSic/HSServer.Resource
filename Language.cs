5f244a4ea4eef576f1793752630ad1c18858420b
ParkHongSic <gpo04174@naver.com>
2021-01-22 05:40:29 +0900

    2021-01-22 First commit

---
+++ Language.cs
@@ -0,0 +1,16 @@
+﻿using HS.Utils;
+using System;
+using System.Globalization;
+
+namespace HSServer.Resource
+{
+    public class Language
+    {
+        public static LanguageManager FromLocale(string Locale = null)
+        {
+            string lngcode = string.IsNullOrWhiteSpace(Locale) ? CultureInfo.CurrentCulture.Name.ToLower() : Locale;
+            if (lngcode.Equals("ko-kr")) return new LanguageManager(Resources.Language_ko_kr.Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries));
+            else return new LanguageManager(Resources.Language_en_us.Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries));
+        }
+    }
+}
